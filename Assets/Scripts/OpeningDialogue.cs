5c9bc63ee867490b814403cfd228de4a0632000f
Javier Vargas <3231javiervargas@gmail.com>
2023-07-07 22:40:10 -0400

    simpler set up for the openning scene

--- Assets/Scripts/OpeningDialogue.cs
+++ Assets/Scripts/OpeningDialogue.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 
 public class OpeningDialogue : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
-    public string[] lines;
+    public Image frame1;
+    public Image frame2;
+    public Image frame3;
+   public string[] lines;
+    
     public float textSpeed;
 
     private int index;
@@ -15,7 +20,7 @@ public class OpeningDialogue : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    {     
         textComponent.text = string.Empty;
         StartDialogue();
     }
@@ -33,20 +38,30 @@ public class OpeningDialogue : MonoBehaviour
         }
     }
 
-    void StartDialogue(){
+    void StartDialogue()
+    {
+
         index = 0;
+
         StartCoroutine(TypeLine());
     }
 
-    IEnumerator TypeLine(){
+    IEnumerator TypeLine()
+    {
         foreach(char c in lines[index].ToCharArray()){
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
     }
 
-    void NextLine(){
-        if (index < lines.Length - 1){
+    void NextLine()
+    {   
+        if(lines[index] == "*NEXT*" ){
+            index++;
+            frame1.enabled = false;
+            frame2.enabled = true;
+        }
+        else if (index < lines.Length - 1 &&  lines[index] != "*NEXT*"){
             index++;
             textComponent.text = string.Empty;
             StartCoroutine(TypeLine());
