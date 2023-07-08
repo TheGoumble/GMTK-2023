e520abd95ca8fed84a145fc3509dc5458df15d76
RatthewThePlagueBringer <matthewgrimaldi111@gmail.com>
2023-07-07 21:25:58 -0400

    hahah

--- Assets/player_fishing.cs
+++ Assets/player_fishing.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
@@ -9,31 +10,27 @@ public class player_fishing : MonoBehaviour
     
     [SerializeField] GameObject rod_go;
     [SerializeField] GameObject pivot_go;
-    private SpriteRenderer rod_sr;
-    float rotat = 0;
+    [SerializeField] GameObject target_a_go;
+    [SerializeField] float rodSpeed;
+    private Quaternion lookRotation;
+    private Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
     {
-        rod_sr = rod_go.GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rod_go.transform.rotation.z > -60)
-        {
-            rod_go.transform.RotateAround(pivot_go.transform.position, new Vector3(0, 0, 1), -0.01f);
-        }
-        //if (rod_go.transform.rotation.z < -60)
-        //{
-        //    rotat = 0;
-        //}
-        //rod_go.transform.RotateAround(pivot_go.transform.position, new Vector3(0,0,1), rotat * Time.deltaTime);
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    rotat = -300;
-        //}
+        direction = new Vector2(target_a_go.transform.position.x - transform.position.x, target_a_go.transform.position.y - transform.position.y).normalized;
+
+        lookRotation = Quaternion.LookRotation(direction);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rodSpeed);
         
+
+
     }
 }
