513427e349d2193a8493bc5f55150de840a6f45c
RatthewThePlagueBringer <matthewgrimaldi111@gmail.com>
2023-07-08 16:57:05 -0400

    fishing biomes

--- Assets/Scripts/FishingMinigame.cs
+++ Assets/Scripts/FishingMinigame.cs
@@ -10,6 +10,10 @@ public class FishingMinigame : MonoBehaviour
     [SerializeField] GameObject fish_go;
     [SerializeField] GameObject slider_go;
     [SerializeField] GameObject splash_go;
+    [SerializeField] GameObject grass_go;
+    [SerializeField] GameObject desert_go;
+    [SerializeField] GameObject snow_go;
+    [SerializeField] GameObject badlands_go;
     private ParticleSystem splash;
     private Rigidbody2D fish_rb;
     private int tier;
@@ -20,6 +24,8 @@ public class FishingMinigame : MonoBehaviour
     public Sprite[] goodFish;
     public Sprite[] perfectFish;
 
+    public string biome;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +34,35 @@ public class FishingMinigame : MonoBehaviour
         jumpable = true;
         fish_rb = fish_go.gameObject.GetComponent<Rigidbody2D>();
         splash = splash_go.GetComponent<ParticleSystem>();
+        biome = biome.ToLower();
+        if (biome == "grass")
+        {
+            grass_go.SetActive(true);
+            desert_go.SetActive(false);
+            snow_go.SetActive(false);
+            badlands_go.SetActive(false);
+        }
+        else if (biome == "desert")
+        {
+            grass_go.SetActive(false);
+            desert_go.SetActive(true);
+            snow_go.SetActive(false);
+            badlands_go.SetActive(false);
+        }
+        else if (biome == "snow")
+        {
+            grass_go.SetActive(false);
+            desert_go.SetActive(false);
+            snow_go.SetActive(true);
+            badlands_go.SetActive(false);
+        }
+        else if (biome == "badlands")
+        {
+            grass_go.SetActive(false);
+            desert_go.SetActive(false);
+            snow_go.SetActive(false);
+            badlands_go.SetActive(true);
+        }
     }
 
     // Update is called once per frame
