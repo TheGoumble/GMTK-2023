5bd03881a652115db20e12d9193e490d972a3374
Andrew Kohler <awk480@gmail.com>
2023-07-08 18:16:08 -0400

    Finished main menu
    EXCEPT FOR CREDITS, main menu is complete

--- Assets/Scripts/Menus and Cutscenes/MainMenu.cs
+++ Assets/Scripts/Menus and Cutscenes/MainMenu.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class MainMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject creditsMenu;
     [SerializeField] private TransitionScreen transitionScreen;
+    [SerializeField] private Image pointer;
+
+    private void Start()
+    {
+        pointer.gameObject.SetActive(false);
+    }
     public void PlayButton()
     {
         StartCoroutine(DoPlayGame());
@@ -34,4 +42,15 @@ public class MainMenu : MonoBehaviour
         yield return new WaitForSeconds(6f);
         SceneManager.LoadScene(1); // Load the opening cutscene
     }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        pointer.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        pointer.gameObject.SetActive(false);
+    }
+
 }
