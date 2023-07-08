90a65e8a595ea05d88ff8416c308887b33aa67fb
Michael <michaelgarcia5656@gmail.com>
2023-07-07 22:35:51 -0400

    UI combat functions

--- Assets/Scripts/GenerateRandomLetter.cs
+++ Assets/Scripts/GenerateRandomLetter.cs
@@ -9,6 +9,14 @@ public class GenerateRandomLetter : MonoBehaviour
     
     // Start is called before the first frame update
     void Awake(){
+        GenerateLetter();
+    }
+    private void Update(){
+        if(!gameObject.activeSelf){
+            GenerateLetter();
+        }
+    }
+    public void GenerateLetter(){
         string alphebet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         c = alphebet[Random.Range(0, alphebet.Length-1)].ToString();
 
